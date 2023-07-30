cbed3e32f1464831a4918074400526b4acd551ea
Roman Nevzorov <pomkanevzorov@gmail.com>
2023-07-30 18:36:29 +0600

    Photo slider v.1.1

--- E5/photo-slider/scripts/slider-script.js
+++ E5/photo-slider/scripts/slider-script.js
@@ -1,44 +1,29 @@
-let slides = document.querySelectorAll('.single-slide');
-console.log(slides);
-let slider = [];
-for (let i=0; i<slides.length; i++) {
-    slider[i] = slides[i].src;
-    slides[i].remove();
-}
-console.log(slider);
+let images = document.querySelectorAll('.single-slide');
+let current = 0;
 
-let step = 0;
-let offset = 0;
+function slider() {
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.add('opacity0')
+    };
+    images[current].classList.remove('opacity0');
+};
 
-function draw() {
-    let img = document.createElement('img');
-    img.src = slider[step];
-    img.classList.add('single-slide');
-    img.style.left = offset * 600 + 'px';
-    document.querySelector('#slide').appendChild(img);
-    if (step + 1 == slider.length) {
-        step = 0;
+slider();
+
+function showNext() {
+    if (current + 1 == images.length) {
+        current = 0;
     } else {
-        step++;
-    }
-    offset = 1;
-}
+    current++;
+    };
+    slider();
+};
 
-function left() {
-    document.onclick = null;
-    let slides2 = document.querySelectorAll('.single-slide');
-    let offset2 = 0;
-    for (let i=0; i<slides2.length; i++) {
-        slides2[i].style.left = offset2 * 600 - 600 + 'px';
-        offset2++;
-    }
-    setTimeout(function() {
-        slides2[0].remove();
-        draw();
-        document.onclick = left;
-    }, 1000);
-}
-
-draw();
-draw();
-document.onclick = left;
+function showPrev() {
+    if (current - 1 == -1) {
+        current = images.length - 1;
+    } else {
+    current--;
+    };
+    slider();
+};
